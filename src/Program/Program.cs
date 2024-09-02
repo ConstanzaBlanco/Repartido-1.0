93f33e8c7b1781b0ab0b311f60c2ac3fefb6315e
Constanza Blanco <constanza.blanco@correo.ucu.edu.uy>
2024-09-02 11:13:35 -0300

    Update Program.cs

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
 
-namespace Ucu.Poo.GameOfLife
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
